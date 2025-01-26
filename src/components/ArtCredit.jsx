197d27af2df7978728e609bae77e39d2c9fc1989
LonliLucas <lukej.gilless@gmail.com>
2025-01-25 18:31:44 -0700

    Added some links and changed art

---
+++ src/components/ArtCredit.jsx
@@ -0,0 +1,3 @@
+export const ArtCredit = function() {
+    
+}
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,17 +1,17 @@
 export const Buttons = function () {
   return (
     <div className="button-container">
-      <button>
+      <button onClick={() => window.open("https://wanderersguide.app/campaign/2041", "_blank", "noopener,noreferrer")}>
         <span className="transition"></span>
         <span className="gradient"></span>
-        <span className="label">Button</span>
+        <span className="label">Wanderer's Guide</span>
       </button>
-      <button>
+      <button onClick={() => window.open("https://foundry.brich.one/", "_blank", "noopener,noreferrer")}>
         <span className="transition"></span>
         <span className="gradient"></span>
-        <span className="label">Button</span>
+        <span className="label">Foundry</span>
       </button>
-      <button>
+      <button onClick={() => window.open("https://example.com", "_blank", "noopener,noreferrer")}>
         <span className="transition"></span>
         <span className="gradient"></span>
         <span className="label">Button</span>
