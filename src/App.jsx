acc91a07b67cc368da18fbc80faa157f96bd0b34
LonliLucas <lukej.gilless@gmail.com>
2025-01-25 18:01:45 -0700

    Added button classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import { useState } from 'react'
 import './App.css';
+import { Buttons } from './components/buttons';
 
 function App() {
   return (
     <>
-      
+      <Buttons />
     </>
   )
 }
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,21 @@
+export const Buttons = function () {
+  return (
+    <div className="button-container">
+      <button>
+        <span className="transition"></span>
+        <span className="gradient"></span>
+        <span className="label">Button</span>
+      </button>
+      <button>
+        <span className="transition"></span>
+        <span className="gradient"></span>
+        <span className="label">Button</span>
+      </button>
+      <button>
+        <span className="transition"></span>
+        <span className="gradient"></span>
+        <span className="label">Button</span>
+      </button>
+    </div>
+  );
+};
