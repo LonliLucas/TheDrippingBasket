2638375aec1c4338f17f6c2cdca65d565e40376f
LonliLucas <lukej.gilless@gmail.com>
2025-04-05 18:35:39 -0600

    Added artist credit link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './App.css';
 import { Buttons } from './components/Buttons';
+import { ArtCredit } from './components/ArtCredit';
 
 function App() {
   return (
     <>
       <Buttons />
+      <ArtCredit />
     </>
   )
 }
--- src/components/ArtCredit.jsx
+++ src/components/ArtCredit.jsx
@@ -1,3 +1,7 @@
-export const ArtCredit = function() {
-    
+export const ArtCredit = function () {
+    return (
+    <div className="art-credit">
+        Background art by <a href="https://www.artstation.com/anto-finnstark" target="_blank" rel="noopener,noreferrer">Anto Finnstark</a>
+    </div>
+    )
 }
